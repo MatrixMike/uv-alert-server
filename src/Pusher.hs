7d9ca13268449809d3c0f25de8a34ea770a0bc7a
Alexey Kotlyarov <a@koterpillar.com>
2015-11-07 21:13:24 +1100

    Fix pins

--- src/Pusher.hs
+++ src/Pusher.hs
@@ -26,42 +26,55 @@ putForecastPin :: Forecast -> AppM ()
 putForecastPin forecast = do
     apiKey <- asks coApiKey
     let topics = forecastTopics forecast
-    let pin = forecastPin forecast
-    result <- liftIO $ runEitherT $ putSharedPin (Just apiKey) (Just topics) (pinId pin) pin
-    case result of
-        Left err -> error $ show err
-        Right result' -> return ()
+    let pins = forecastPin forecast
+    forM_ pins $ \pin -> do
+        logStr $ show $ A.encode pin
+        result <- liftIO $ runEitherT $ putSharedPin (Just apiKey) (Just topics) (pinId pin) pin
+        case result of
+            Left err -> error $ show err
+            Right result' -> return ()
 
-forecastPin :: Forecast -> Pin
-forecastPin forecast@Forecast{..} = Pin { pinId = pinId
-                                        , pinTime = fcStartTimeUtc forecast
-                                        , pinDuration = Just $ fcDuration forecast
-                                        , pinCreateNotification = Just createNotification
-                                        , pinUpdateNotification = Just updateNotification
-                                        , pinLayout = layout
-                                        , pinReminders = []
-                                        , pinActions = []
-                                        }
-    where layout = Layout { layoutType = WeatherPin
-                          , layoutTitle = "UV level: " ++ show (uvValue maxLevel)
-                          , layoutSubtitle = Just $ "From: " ++ showTime alertStart ++ " To: " ++ showTime alertEnd
-                          , layoutBody = Nothing
-                          , layoutTinyIcon = Just "system://images/TIMELINE_SUN"
-                          , layoutSmallIcon = Nothing
-                          , layoutLargeIcon = Nothing
-                          , layoutPrimaryColor = Nothing
-                          , layoutSecondaryColor = Nothing
-                          , layoutBackgroundColor = Nothing
-                          , layoutParagraphs = []
-                          , layoutLastUpdated = Nothing
-                          }
-          createNotification = Notification { notificationLayout = layout
-                                            , notificationTime = Nothing
-                                            }
-          updateNotification = Notification { notificationLayout = layout
-                                            , notificationTime = Just $ fcStartTimeUtc forecast
-                                            }
-          pinId = replaceSpace $ city location ++ show date
+forecastPin :: Forecast -> [Pin]
+forecastPin forecast@Forecast{..} = [ Pin { pinId = pinId ++ "start"
+                                          , pinTime = fcStartTimeUtc forecast
+                                          , pinDuration = Nothing
+                                          , pinCreateNotification = Nothing
+                                          , pinUpdateNotification = Nothing
+                                          , pinLayout = startLayout
+                                          , pinReminders = []
+                                          , pinActions = []
+                                          }
+                                    , Pin { pinId = pinId ++ "end"
+                                          , pinTime = fcEndTimeUtc forecast
+                                          , pinDuration = Nothing
+                                          , pinCreateNotification = Nothing
+                                          , pinUpdateNotification = Nothing
+                                          , pinLayout = endLayout
+                                          , pinReminders = []
+                                          , pinActions = []
+                                          }
+                                    ]
+    where
+        baseLayout = Layout { layoutType = GenericPin
+                            , layoutTitle = ""
+                            , layoutSubtitle = Just $ "Max level: " ++ show (uvValue maxLevel)
+                            , layoutBody = Just $ "Alert from " ++ showTime alertStart ++ " to " ++ showTime alertEnd
+                            , layoutTinyIcon = Nothing
+                            , layoutSmallIcon = Nothing
+                            , layoutLargeIcon = Nothing
+                            , layoutPrimaryColor = Nothing
+                            , layoutSecondaryColor = Nothing
+                            , layoutBackgroundColor = Nothing
+                            , layoutParagraphs = []
+                            , layoutLastUpdated = Nothing
+                            }
+        startLayout = baseLayout { layoutTitle = "UV Alert start"
+                                 , layoutTinyIcon = Just "system://images/TIMELINE_SUN"
+                                 }
+        endLayout = baseLayout { layoutTitle = "UV Alert end"
+                               , layoutTinyIcon = Just "system://images/TIMELINE_SUN"
+                               }
+        pinId = replaceSpace $ city location ++ show date
 
 forecastTopics :: Forecast -> Topics
 forecastTopics forecast = Topics [locTopic]
