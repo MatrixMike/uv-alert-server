8f49555c05e62e29d222276c6776c290b610cc15
Alexey Kotlyarov <a@koterpillar.com>
2016-01-23 13:42:07 +1100

    Set stdout to line buffering

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,15 @@ import Control.Concurrent
 
 import Network.Wai.Handler.Warp
 
+import System.IO
+
 import App
 import Fetcher
 import Server
 
 main :: IO ()
 main = do
+    hSetBuffering stdout LineBuffering
     config <- initConfig
     let theApp = app config
     forkIO $ runFetcher config
