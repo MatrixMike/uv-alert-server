744cdc811c7f77830aa251273a4df1ae3cd206e8
Alexey Kotlyarov <a@koterpillar.com>
2016-01-26 11:02:22 +1100

    Move BOM-specific parsing to the BOM fetcher

--- src/Fetcher/BOM.hs
+++ src/Fetcher/BOM.hs
@@ -17,7 +17,10 @@ import Control.Monad.Trans.Reader
 
 import Data.Either
 import qualified Data.Set as S
+import Data.Time.Calendar
 import Data.Time.Clock
+import Data.Time.LocalTime
+import Data.Time.LocalTime.TimeZone.Series
 
 import Network.FTP.Client
 import Network.URI
@@ -57,3 +60,45 @@ fetchTestContent = liftIO $ readFile "src/IDYGP007.txt"
 bomFetchers = [ Fetcher "BOM today" (fetchBOM todayAddress)
               , Fetcher "BOM forecast" (fetchBOM forecastAddress)
               ]
+
+parseDate :: String -> Either String Day
+parseDate str = do
+    day <- readEither "day" $ stringPartT 0 2 str
+    month <- readEither "month" $ stringPartT 3 2 str
+    year <- readEither "year" $ stringPartT 6 4 str
+    let maybeDate = fromGregorianValid year month day
+    case maybeDate of
+        Just date -> return date
+        Nothing -> error "Invalid date"
+
+parseTime :: String -> Either String TimeOfDay
+parseTime str = do
+    hour <- readEither "hour" $ stringPartT 0 2 str
+    minute <- readEither "minute" $ stringPartT 3 2 str
+    return $ TimeOfDay hour minute 0
+
+-- TODO: Parsec
+parseForecast :: UTCTime -> String -> Either String Forecast
+parseForecast updated str = do
+    let location = Location $ stringPartT 18 20 str
+    date <- parseDate $ stringPart 38 10 str
+    tStart <- parseTime $ stringPart 64 5 str
+    tEnd <- parseTime $ stringPart 73 5 str
+    max <- liftM UVLevel $ readEither "UV level" $ stringPartT 84 3 str
+    return $ Forecast location date tStart tEnd max updated
+
+trim :: String -> String
+trim = trimStart . trimEnd
+    where trimStart = dropWhile (== ' ')
+          trimEnd = reverse . trimStart . reverse
+
+stringPart :: Int -> Int -> String -> String
+stringPart start len = take len . drop start
+
+stringPartT :: Int -> Int -> String -> String
+stringPartT start len = trim . stringPart start len
+
+readEither :: Read a => e -> String -> Either e a
+readEither err str = case reads str of
+    [(res, "")] -> Right res
+    _ -> Left err
